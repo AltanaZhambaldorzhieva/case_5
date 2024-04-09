0c3c607ba743370b832cba1b77a033475e50a864
daryazaitseva <zaytsevaart@gmail.com>
2024-04-10 01:18:13 +0700

    changes

--- main.py
+++ main.py
@@ -13,63 +13,64 @@ def random_granny():
 def floor_2():
     health = 3
     force = 5
-    print(f'{ru.CHOOSE}:\n1.{ru.GO}\n2.{ru.ENTER}')
-    act = int(input(f'{ru.ACT}->'))
-    if act == 1:
-        if random_granny() == 2:
-            print(f'{ru.OH_OH}')
-            health -= 1
-            force -= 2
-        print(f'{ru.CHOOSE}:\n1.{ru.GO_TO_ROOM_1}\n2.{ru.GO_TO_ROOM_2}')
-        act_1 = int(input(f'{ru.ACT}->'))
-        if act_1 == 1:
-            print(f'{ru.CHOOSE}:\n1.{ru.OPEN_WARDROBE}\n2.{ru.OPEN_FRIDGE}')
-            act_2 = int(input(f'{ru.ACT}->'))
-            if act_2 == 1:
-                print(ru.EMPTY)
-            else:
-                print(ru.SPRAY)
-        else:
-            print(ru.EMPTY)
-        print(f'{ru.CHOOSE}:\n1.{ru.GO}\n2.{ru.GO_TO_ROOM_1}')
-        act_3 = int(input(f'{ru.ACT}->'))
-        if act_3 == 1:
+    for k in names:
+        print(f'{k}, {ru.CHOOSE}:\n1.{ru.GO}\n2.{ru.ENTER}')
+        act = int(input(f'{ru.ACT}->'))
+        if act == 1:
             if random_granny() == 2:
                 print(f'{ru.OH_OH}')
                 health -= 1
                 force -= 2
-            print(f'{ru.CHOOSE}:\n1.{ru.GO}\n2.{ru.GO_TO_ROOM_3}')
-            act_4 = int(input(f'{ru.ACT}->'))
-            if act_4 == 1:
+            print(f'{k}, {ru.CHOOSE}:\n1.{ru.GO_TO_ROOM_1}\n2.{ru.GO_TO_ROOM_2}')
+            act_1 = int(input(f'{ru.ACT}->'))
+            if act_1 == 1:
+                print(f'{k}, {ru.CHOOSE}:\n1.{ru.OPEN_WARDROBE}\n2.{ru.OPEN_FRIDGE}')
+                act_2 = int(input(f'{ru.ACT}->'))
+                if act_2 == 1:
+                    print(ru.EMPTY)
+                else:
+                    print(ru.SPRAY)
+            else:
+                print(ru.EMPTY)
+            print(f'{k}, {ru.CHOOSE}:\n1.{ru.GO}\n2.{ru.GO_TO_ROOM_1}')
+            act_3 = int(input(f'{ru.ACT}->'))
+            if act_3 == 1:
                 if random_granny() == 2:
                     print(f'{ru.OH_OH}')
                     health -= 1
                     force -= 2
-            else:
-                print(f'{ru.CHOOSE}:\n1.{ru.OPEN_WARDROBE}\n2.{ru.OPEN_BAG}')
-                act_5 = int(input(f'{ru.ACT}->'))
-                if act_5 == 1:
-                    print(f'{ru.MEDICINE}')
-                    health += 1
-                    if health > 3:
-                        health = 3
+                print(f'{k}, {ru.CHOOSE}:\n1.{ru.GO}\n2.{ru.GO_TO_ROOM_3}')
+                act_4 = int(input(f'{ru.ACT}->'))
+                if act_4 == 1:
+                    if random_granny() == 2:
+                        print(f'{ru.OH_OH}')
+                        health -= 1
+                        force -= 2
                 else:
-                    print(f'{ru.EMPTY}')
-        else:
-            print(f'{ru.CHOOSE}:\n1.{ru.OPEN_WARDROBE}\n2.{ru.OPEN_FRIDGE}')
-            act_2 = int(input(f'{ru.ACT}->'))
-            if act_2 == 1:
-                print(f'{ru.EMPTY}')
+                    print(f'{k}, {ru.CHOOSE}:\n1.{ru.OPEN_WARDROBE}\n2.{ru.OPEN_BAG}')
+                    act_5 = int(input(f'{ru.ACT}->'))
+                    if act_5 == 1:
+                        print(f'{ru.MEDICINE}')
+                        health += 1
+                        if health > 3:
+                            health = 3
+                    else:
+                        print(f'{ru.EMPTY}')
             else:
-                print(f'{ru.SPRAY}')
-    else:
-        print(f'{ru.CHOOSE}:\n1.{ru.OPEN_WARDROBE}\n2.{ru.OPEN_BAG}')
-        act_5 = int(input(f'{ru.ACT}->'))
-        if act_5 == 1:
-            print(f'{ru.MEDICINE}')
-            health += 1
-            if health > 3:
-                health = 3
+                print(f'{k}, {ru.CHOOSE}:\n1.{ru.OPEN_WARDROBE}\n2.{ru.OPEN_FRIDGE}')
+                act_2 = int(input(f'{ru.ACT}->'))
+                if act_2 == 1:
+                    print(f'{ru.EMPTY}')
+                else:
+                    print(f'{ru.SPRAY}')
+        else:
+            print(f'{k}, {ru.CHOOSE}:\n1.{ru.OPEN_WARDROBE}\n2.{ru.OPEN_BAG}')
+            act_5 = int(input(f'{ru.ACT}->'))
+            if act_5 == 1:
+                print(f'{ru.MEDICINE}')
+                health += 1
+                if health > 3:
+                    health = 3
     return force
 
 
