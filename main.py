669b544ddffbf9554b90a3351c4ad306844b9406
Darialr <darial.ryaguzova@gmail.com>
2024-04-09 21:20:03 +0700

    for

--- main.py
+++ main.py
@@ -3,19 +3,16 @@
 #
 import ru_local as ru
 
-player1 = input(f'{ru.NAME} 1->')
-player2 = input(f'{ru.NAME} 2->')
-player3 = input(f'{ru.NAME} 3->')
-player4 = input(f'{ru.NAME} 4->')
+names = []
+for i in range(4):
+    name = input(f'{ru.NAME} {i+1}->')
+    names.append(name)
 
 print(ru.INITIAL_INFO)
 print(f'{ru.RESOURCES}:\n{ru.HEALTH} 3\n{ru.FORCE} 5')
 
-print(f'{player1}, {ru.CHOOSE}:\n1.{ru.DOWN}\n2.{ru.GO}\n3.{ru.ENTER}')
-act1 = int(input(f'{ru.ACT}->'))
-print(f'{player2}, {ru.CHOOSE}:\n1.{ru.DOWN}\n2.{ru.GO}\n3.{ru.ENTER}')
-act2 = int(input(f'{ru.ACT}->'))
-print(f'{player3}, {ru.CHOOSE}:\n1.{ru.DOWN}\n2.{ru.GO}\n3.{ru.ENTER}')
-act3 = int(input(f'{ru.ACT}->'))
-print(f'{player4}, {ru.CHOOSE}:\n1.{ru.DOWN}\n2.{ru.GO}\n3.{ru.ENTER}')
-act4 = int(input(f'{ru.ACT}->'))
+act1 = []
+for i in names:
+    print(f'{i}, {ru.CHOOSE}:\n1.{ru.DOWN}\n2.{ru.GO}\n3.{ru.ENTER}')
+    action1 = int(input(f'{ru.ACT}->'))
+    act1.append(action1)
